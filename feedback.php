54976da0edf649f9f7f04d08f0fc19bed0171edb
AnsonDev <65697201+anson923@users.noreply.github.com>
2022-12-03 15:28:53 -0800

    Added Upload and Receive data from SQL
    WIP- Image is still in progress

--- feedback.php
+++ feedback.php
@@ -1,5 +1,42 @@
 <?php include 'include/header.php' ?>
 
+<?php
 
+  $sql = "SELECT * FROM feedback_app ORDER BY id DESC";
+  if($result = mysqli_query($conn,$sql))
+  {
+    $data = $result->fetch_all(MYSQLI_ASSOC);
+  }
+
+
+?>
+
+<?php if(!empty($data)): ?>
+
+  <?php foreach($data as $row): ?>
+
+    <div class="feedback-block">
+      <!-- Add image if exist -->
+      <?php if(empty($row['image'])): ?>
+      <div class="feedback-image">
+        <img class="image" src="res/Yummy_Restaurant_Banner.png"/>
+      </div>
+      <?php endif; ?>
+
+      <!-- Feedback -->
+      <div class="feedback-content">
+        <?php echo $row['feedback']; ?>
+      </div>
+
+      <!-- By who and date -->
+      <div class="feedback-by">
+        By <?php echo $row['name']; ?> at <?php echo $row['date'];?>
+      </div>
+
+    </div>
+
+  <?php endforeach; ?>
+
+<?php endif; ?>
 
 <?php include 'include/footer.php' ?>
--- index.php
+++ index.php
@@ -1,17 +1,86 @@
+<?php include 'include/header.php' ?>
+
 <?php
 
-  //POST submit
+//POST submit
+$allowed_ext = array('png', 'jpg', 'jpeg', 'gif');
 
+if (isset($_POST['submit'])) {
+  $name = $nameErr = $email = $feedback = $feedbackErr = $image = $imageErr = "";
 
+  if (!empty($_FILES['upload'])) {
+    //Check extension
+    $path = $_FILES['upload']['name'];
+    $ext = pathinfo($path, PATHINFO_EXTENSION);
+    if (in_array($ext, $allowed_ext)) {
+      $file_tmp = $_FILES['upload']['tmp_name'];
+      $img = file_get_contents($file_tmp);
+      $data = base64_encode($img); 
+    }
+  }
+
+  if (!empty($_POST['name']) && !empty($_POST['feedback'])) {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $feedback = filter_input(INPUT_POST, 'feedback', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $sql = "INSERT INTO feedback_app (name,email,feedback,image) VALUES ('$name', '$email', '$feedback', '$image')";
+
+    if($result = mysqli_query($conn,$sql))
+    {
+      header('Location: feedback.php');
+    }
+  } 
+  else 
+  {
+    $nameErr = empty($_POST['name']) ? "<p style='color:red'> Name cannot be empty </p>" : "";
+    $feedbackErr = empty($_POST['feedback']) ? "<p style='color:red'> Feedback cannot be empty </p>" : "";
+  
+  }
+}
 
 ?>
 
-
-<?php include 'include/header.php' ?>
 <div class="feedback-form">
   <div class="banner">
-    <img class="banner-img" src="res/Yummy_Restaurant_banner.png" alt="Yummy Restaurant banner"/>
+    <img class="banner-img" src="res/Yummy_Restaurant_Banner.png" alt="Yummy Restaurant banner" />
   </div>
+
+  <div>
+    <form class="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+      <div class="form-element">
+        <label for="name" class="form-label"> *Name:</label>
+        <input type="text" class="form-control <?php echo !$nameErr ?: 'is-invalid'; ?>" placeholder="Enter Your Name" name="name">
+        <div class="is-invalid">
+          <?php echo $nameErr ?? null ?>
+        </div>
+      </div>
+
+      <div class="form-element">
+        <label for="email" class="form-label"> Email:</label>
+        <input type="email" class="form-control" placeholder="Enter Your Email" name="email">
+      </div>
+
+      <div class="form-element">
+        <label for="feedback" class="form-label"> *Feedback:</label>
+        <textarea name="feedback" class="form-control <?php echo !$nameErr ?: 'is-invalid'; ?>" name="feedback"></textarea>
+        <div class="is-invalid">
+          <?php echo $feedbackErr ?? null ?>
+        </div>
+      </div>
+
+      <div class="form-element">
+        <label for="upload" class="form-label"> Food Image:</label>
+        <input type="file" value="Upload Image" name="upload">
+      </div>
+
+
+      <input class="submit" type="submit" value="Submit" name="submit">
+    </form>
+  </div>
+
+
+
 </div>
 
 
